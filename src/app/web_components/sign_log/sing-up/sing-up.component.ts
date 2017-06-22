f4a66867287e10f1042e86a2234f5bf317c2b281
vinkky <nezinaugoblinas5@gmail.com>
2017-06-22 18:45:46 +0300

    created registration full validation

--- src/app/web_components/sign_log/sing-up/sing-up.component.ts
+++ src/app/web_components/sign_log/sing-up/sing-up.component.ts
@@ -26,12 +26,79 @@ form: FormGroup;
      
   {
      this.form = fb.group({
-      password: ['', Validators.required],
-      cPassword: ['', Validators.required]
+      
+      name: ['', Validators.compose([Validators.required,
+       Validators.minLength(5), // Minimum length is 5 characters
+      Validators.maxLength(30), // Maximum length is 30 characters
+      this.validateUsername])],
+      surname: ['', Validators.compose([Validators.required,
+       Validators.minLength(8), // Minimum length is 5 characters
+      Validators.maxLength(35), // Maximum length is 30 characters
+      this.validateUsername])],
+      password: ['', Validators.compose([
+        Validators.required, // Field is required
+        Validators.minLength(8), // Minimum length is 8 characters
+        Validators.maxLength(35), // Maximum length is 35 characters
+        this.validateUsername // Custom validation
+      ])],
+      cPassword: ['', Validators.required],// Confirm Password Input
+      email:['', Validators.compose([Validators.required,
+       Validators.minLength(5), // Minimum length is 5 characters
+      Validators.maxLength(30), // Maximum length is 30 characters
+      this.validateEmail])]
+
     }, {
-      validator: PasswordValidation.MatchPassword // your validation method
+      validator: this.matchingPasswords('password', 'cPassword') // your validation method
     })
   }
+  // Function to validate e-mail is proper format
+  validateEmail(controls) {
+    // Create a regular expression
+    const regExp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+    // Test email against regular expression
+    if (regExp.test(controls.value)) {
+      return null; // Return as valid email
+    } else {
+      return { 'validateEmail': true } // Return as invalid email
+    }
+  }
+
+  // Function to validate username is proper format
+  validateUsername(controls) {
+    // Create a regular expression
+    const regExp = new RegExp(/^[a-zA-Z0-9]+$/);
+    // Test username against regular expression
+    if (regExp.test(controls.value)) {
+      return null; // Return as valid username
+    } else {
+      return { 'validateUsername': true } // Return as invalid username
+    }
+  }
+
+  // Function to validate password
+  validatePassword(controls) {
+    // Create a regular expression
+    const regExp = new RegExp(/^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])(?=.*?[\W]).{8,35}$/);
+    // Test password against regular expression
+    if (regExp.test(controls.value)) {
+      return null; // Return as valid password
+    } else {
+      return { 'validatePassword': true } // Return as invalid password
+    }
+  }
+
+  // Funciton to ensure passwords match
+  matchingPasswords(password, confirm) {
+    return (group: FormGroup) => {
+      // Check if both fields are the same
+      if (group.controls[password].value === group.controls[confirm].value) {
+        return null; // Return as a match
+      } else {
+        return { 'matchingPasswords': true } // Return as error: do not match
+      }
+    }
+  }
+
     
 //private formBuilder: FormBuilder
 
