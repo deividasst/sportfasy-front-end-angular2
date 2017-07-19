525d98df6aec98b35729675751525b531af6cdd7
vinkky <nezinaugoblinas5@gmail.com>
2017-07-19 16:16:31 +0300

    added scripts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,14 @@
+// production evironment
 export const environment = {
     production: true,
     origin: 'http://localhost:3001'
 };
+// #build
+// $ ng build --environment=production
+// #shorthand
+// $ ng b -prod
+//
+// #serve
+// $ ng serve --environment=production
+// #shorthand
+// $ ng s -prod
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,18 @@
 // The file contents for the current environment will overwrite these during build.
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// `ng build --env=dev` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
   origin: 'http://localhost:3000',
     swagger: 'https://app.swaggerhub.com/apis/markrudenkov/sportfasy/1.0.0'
 };
+// #build
+// $ ng build --environment=dev
+// #shorthand
+// $ ng b -dev
+//
+// #serve
+// $ ng serve --environment=dev
+// #shorthand
+// $ ng s -dev
