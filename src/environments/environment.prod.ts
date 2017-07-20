39dc7ed2803b0814e11f68c66e2633970c686624
markrudenkov <mark.rudenkov@gmail.com>
2017-07-20 16:09:40 +0300

    hotfix enviroments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,4 @@
-// production evironment
 export const environment = {
     production: true,
     origin: 'http://http://88.119.200.69:3001'
-};
-// #build
-// $ ng build --environment=production
-// #shorthand
-// $ ng b -prod
-//
-// #serve
-// $ ng serve --environment=production
-// #shorthand
-// $ ng s -prod
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,3 @@
-
-
 export const environment = {
   origin: 'http://localhost:3000'
 };
