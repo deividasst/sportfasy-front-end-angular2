f882464d2d8ae082f6ebf21f9cf556baea06992e
vinkky <nezinaugoblinas5@gmail.com>
2017-07-23 13:57:26 +0300

    added players schema

--- src/app/web_components/game_components/team/create-team/create-team.component.ts
+++ src/app/web_components/game_components/team/create-team/create-team.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
+import {DService} from '../../../shared/data.srv';
+import {Router} from '@angular/router';
+import {TokenHolderServise} from '../../../shared/tokenholder.srv';
+import {LogOutComponent} from '../../../sign_log/log-out/log-out.component';
+import {Team} from '../../../shared/Team'
 @Component({
   selector: 'app-create-team',
   templateUrl: './create-team.component.html',
---
+++ src/app/web_components/shared/Players.ts
@@ -0,0 +1,9 @@
+export class Team {
+    name: string;
+    surname: string;
+    age: number;
+    nationality:string;
+    real_team: string;
+    eff_points: number;
+    price: number;
+}
