a4703091d27321e8d872bc318cd32f56f8957528
deividasst <stancikasdeividas@gmail.com>
2017-07-24 17:47:42 +0300

    hotfix: team list fix

--- src/app/web_components/game_components/team/list-team/list-team.component.ts
+++ src/app/web_components/game_components/team/list-team/list-team.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Team} from '../../../shared/Team'
+import {DService} from '../../../shared/data.srv';
 
 @Component({
-  selector: 'app-list-team',
-  templateUrl: './list-team.component.html',
-  styleUrls: ['./list-team.component.sass']
+    selector: 'app-list-team',
+    templateUrl: './list-team.component.html',
+    styleUrls: ['./list-team.component.sass']
 })
 export class ListTeamComponent implements OnInit {
 
-  constructor() { }
+    teams: Team[];
 
-  ngOnInit() {
-  }
+    constructor(private ds: DService) {
+    }
+
+    getTeams(): void {
+        this.ds.getAllTeams().subscribe(team => {
+            this.teams = team
+        })
+    }
+
+    ngOnInit() {
+        this.getTeams();
+    }
 
 }
