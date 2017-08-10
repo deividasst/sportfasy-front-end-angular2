b19417107d46e6b42918297e9f56274eb20c6bdc
unknown <stancikasdeividas@gmail.com>
2017-08-10 11:23:40 +0300

    centered forms

--- src/app/web_components/user-profile/user-profile.component.ts
+++ src/app/web_components/user-profile/user-profile.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, Input, OnInit} from '@angular/core';
+import {GridDataResult, PageChangeEvent} from '@progress/kendo-angular-grid';
+import {MdDialog} from '@angular/material';
+import {Tournament} from '../shared/Tournament';
+import {DService} from '../shared/data.srv';
+import {KendoUiSettings} from '../shared/kendo-ui-settings.srv';
+import {SecurityTrimming} from '../shared/security-trimming.srv';
+import {TournamentDialogComponent} from '../game_components/tournment/tournament-dialog/tournament-dialog.component';
+
+
 
 @Component({
   selector: 'app-user-profile',
@@ -7,9 +16,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileComponent implements OnInit {
 
-  constructor() { }
+    // Kendo grid params
+    gridView: GridDataResult;
+    data: Object[];
+    pageSize: number;
+    skip: number;
 
-  ngOnInit() {
-  }
+    @Input()
+    tournaments: Tournament[];
+
+    constructor(private ds: DService,
+                public dialog: MdDialog,
+                private secureTrim: SecurityTrimming,
+                private  kendoSettings: KendoUiSettings) {
+        this.pageSize = this.kendoSettings.getPageSize();
+        this.skip = this.kendoSettings.getSkip();
+    }
+
+    openDialog(tournament) {
+
+        const dialogRef = this.dialog.open(TournamentDialogComponent, {
+            data: tournament,
+            height: '600px',
+            width: '600px'
+        });
+    }
+
+
+    protected pageChange(event: PageChangeEvent): void {
+        this.skip = event.skip;
+        this.loadItems();
+    }
+
+    private loadItems(): void {
+        this.gridView = {
+            data: this.tournaments.slice(this.skip, this.skip + this.pageSize),
+            total: this.tournaments.length
+        };
+    }
+
+    ngOnInit() {
+        this.loadItems();
+    }
 
 }
